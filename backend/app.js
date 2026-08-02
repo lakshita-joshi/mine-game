1f2395138c1f02e60f28200ab83141dfae98ab0d
Lakshita <lakshita.joshi134@gmail.com>
2026-08-03 00:24:37 +0530

    api changes

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: process.env.CLIENT_ORIGIN ?? "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "http://mine-game-lovat.vercel.app", credentials: true }));
 app.use('/api/auth', authRoutes);
 app.use("/api/mines", minesRoutes);
 
